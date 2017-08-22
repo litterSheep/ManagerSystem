79da6c8ccee6f904c7617fb602aa19f09612cab5
ly <614303432@qq.com>
2017-08-22 18:23:11 +0800

    0822

--- src/main/java/com/xinzhi/system/entity/UserInfo.java
+++ src/main/java/com/xinzhi/system/entity/UserInfo.java
@@ -1,5 +1,7 @@
 package com.xinzhi.system.entity;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
@@ -10,9 +12,11 @@ public class UserInfo implements Serializable {
     @GeneratedValue
     private Integer uid;
     @Column(unique = true)
+    @NotEmpty(message = "请输入用户名")
     private String username;//帐号
 
     private String name;//名称（昵称或者真实姓名，不同系统不同定义）
+    @NotEmpty(message = "请输入密码")
     private String password; //密码;
     private String salt;//加密密码的盐
     private byte state;//用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
